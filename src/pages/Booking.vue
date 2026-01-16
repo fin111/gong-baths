fd653d5ac35f37c48c344dd826f4afd5058f0103
Nathan Croawell <nathan@the49.com>
2026-01-16 10:37:59 +0000

    removed unwanted code

--- src/pages/Booking.vue
+++ src/pages/Booking.vue
@@ -1,36 +1,13 @@
 <template>
     <section class="booking">
         <h1>Book a Session</h1>
-        <!-- <form @submit.prevent="submit">
-            <label>
-                Name:
-                <input v-model="name" required />
-            </label>
-            <label>
-                Email:
-                <input v-model="email" type="email" required />
-            </label>
-            <label>
-                Preferred Date:
-                <input v-model="date" type="date" required />
-            </label>
-            <button type="submit">Send Booking</button>
-        </form> -->
         <BookingList />
     </section>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
 import BookingList from '@/components/BookingList.vue';
 
-const name = ref("");
-const email = ref("");
-const date = ref("");
-
-const submit = () => {
-    alert(`Booking submitted for ${name.value} on ${date.value}`);
-};
 </script>
 
 <style scoped lang="scss">
